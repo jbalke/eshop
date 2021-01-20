f7d3c2eefab305092419bf94a5c0901473ab25f3
John Balke <john.balke@gmail.com>
2021-01-20 14:30:32 +0000

    TailwindCSS setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
+      <Header />
       <h1>Welcome To E-Shop</h1>
+      <Footer />
     </>
   );
 }
