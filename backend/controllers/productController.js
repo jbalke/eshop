8f5795038335132c2eada8452c2293544855eaa6
John Balke <john.balke@gmail.com>
2021-01-31 17:43:24 +0000

    Product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import Product from '../models/productModel.js';
+import asyncHandler from 'express-async-handler';
+
+// @desc     Fetch all products
+// @route    GET /api/products
+// @access   Public
+export const getProducts = asyncHandler(async (req, res) => {
+  const allProducts = await Product.find({}, null, { lean: true });
+  res.json(allProducts);
+});
+
+// @desc     Fetch single product
+// @route    GET /api/product/:id
+// @access   Public
+export const getProductById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id, null, { lean: true });
+
+  if (!product) {
+    res.status(404);
+    throw new Error(`product with id ${id} not found`);
+  }
+  res.json(product);
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,12 @@
 import express from 'express';
-import Product from '../models/productModel.js';
-import asyncHandler from 'express-async-handler';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
-// @desc     Fetch all products
-// @route    GET /api/products
-// @access   Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const allProducts = await Product.find({}, null, { lean: true });
-    res.json(allProducts);
-  })
-);
-
-// @desc     Fetch single product
-// @route    GET /api/product/:id
-// @access   Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id, null, { lean: true });
-
-    if (!product) {
-      res.status(404);
-      throw new Error(`product with id ${id} not found`);
-    }
-    res.json(product);
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
