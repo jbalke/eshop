bc6d528de8bf9dd14f85570a0cee7721f25589bc
John Balke <john.balke@gmail.com>
2021-02-02 12:35:37 +0000

    Profile page

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import ApiService from '../api/ApiService';
+import { useQuery } from 'react-query';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const Profile = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const { isError, error, data, isLoading } = useQuery(
+    'user',
+    ApiService.users.userProfile,
+    { retry: false }
+  );
+
+  useEffect(() => {
+    if (isError) {
+      history.replace(`/login?redirect=${location.pathname}`);
+    }
+  }, [isError, location, history]);
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message type='danger'>{error.message}</Message>
+      ) : (
+        <Message type='success'>{`Profile for user ${data.name}`}</Message>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
