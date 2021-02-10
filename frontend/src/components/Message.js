12fdd257ba79462ed5590c3699a3829540515a1e
John Balke <john.balke@gmail.com>
2021-02-10 14:15:18 +0000

    Embedded variant classes

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,7 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Message = ({ type = 'info', children }) => {
-  return <div className={`message-box ${type} my-4`}>{children}</div>;
+  let classes;
+  switch (type) {
+    case 'danger':
+      classes = 'bg-red-100 text-red-900';
+      break;
+    case 'success':
+      classes = 'bg-green-100 text-green-900';
+      break;
+    case 'warning':
+      classes = 'bg-yellow-100 text-yellow-900';
+      break;
+    default:
+      classes = 'bg-blue-100 text-blue-900';
+      break;
+  }
+  return (
+    <div className={`${classes} tracking-wider p-4 my-4 font-semibold`}>
+      {children}
+    </div>
+  );
 };
 
 Message.propTypes = {
