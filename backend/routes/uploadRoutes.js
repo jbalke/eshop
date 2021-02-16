16d0ed8e660e954483466d7d895569642e0801ac
John Balke <john.balke@gmail.com>
2021-02-16 15:24:19 +0000

    Image uploads

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import { requireAuth, isAdmin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  }
+
+  cb(new Error('Only JPG and PNG supported!'));
+}
+
+const upload = multer({
+  storage,
+  fileFilter(req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post('/', requireAuth, isAdmin, upload.single('image'), (req, res) => {
+  // convert back slashes on windows
+  const path = req.file.path.replace(/\\/g, '/');
+  res.send(`/${path}`);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import connectDatabase from './config/db.js';
 import helmet from 'helmet';
@@ -7,6 +8,7 @@ import productsRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import tokenRoutes from './routes/tokenRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 
@@ -24,10 +26,20 @@ app.use('/api/products', productsRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/auth/token', tokenRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// __dirname is not defined when using es modules with nodejs
+// https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag/51118243#51118243
+const __dirname = (() => {
+  let x = path.dirname(decodeURI(new URL(import.meta.url).pathname));
+  return path.resolve(process.platform == 'win32' ? x.substr(1) : x);
+})();
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 //* ERROR HANDLERS
 app.use(notFound);
