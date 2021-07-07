e1f8b792c4c14965b967f34b31f493cab846b884
John Balke <john.balke@gmail.com>
2021-07-07 11:28:38 +0100

    wip: return history

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,18 +1,18 @@
-import { render } from '../test-utils';
+import { render, renderWithRouter } from '../test-utils';
 import App from './App';
 
 jest.mock('./pages/Home', () => () => <div>Home</div>);
 
 describe('App', () => {
   it('renders successfully', () => {
-    const { container } = render(<App />);
+    const { container } = renderWithRouter(<App />);
     expect(container.innerHTML).toMatch(/E-Shop/i);
   });
 });
 
 describe('routing', () => {
   it('renders Home component on root route', () => {
-    const { container } = render(<App />, { route: '/' });
+    const { container } = renderWithRouter(<App />, { route: '/' });
     expect(container.innerHTML).toMatch('Home');
   });
 });
--- frontend/test-utils.jsx
+++ frontend/test-utils.jsx
@@ -2,26 +2,37 @@ import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 const queryClient = new QueryClient();
 
 const AllTheProviders = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
 
 // test utils file
-const customRender = (ui, { route = '/', ...options } = {}) => {
-  window.history.pushState({}, 'Test page', route);
+const renderWithRouter = (
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+    ...options
+  } = {}
+) => ({
+  ...render(<Router history={history}>{ui}</Router>, {
+    wrapper: AllTheProviders,
+    ...options,
+  }),
+  history,
+});
 
-  return render(ui, { wrapper: AllTheProviders, ...options });
-};
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
 
 // re-export everything
 export * from '@testing-library/react';
 // override render method
-export { customRender as render };
+export { customRender as render, renderWithRouter };
